e1d4189e722aa09292fcf2ed971c1321d300c442
fabiomedda <meddafabio@gmail.com>
2020-12-16 12:12:42 +0100

    inserimento nuova frase

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+/*
+Funzionalitá:
+La nostra todo list avrá alcune tasks di default predefinite
+L'utente puó inserire nuove tasks
+Cliccando sulla "X" l'utente puó cancellare una task
+Se non ci sono piu task nella lista, mostrate un messaggio 
+tipo "Nulla da fare"
+Quando l'utente inserisce una task ha due modi per salvarla: 
+o preme il pulsante add o preme il taso Enter della tastiera.
+Attenzione: l'utente non deve inserire tasks vuote ma almeno 
+un tot di caratteri.
+*/
+
+let root = new Vue ({
+    el: "#root",
+    data: {
+        frasi: [
+            "Do something",
+            "Do else",
+            "Do something else"
+        ],
+        message: ""
+    },
+    methods: {
+        newFrase(){
+            if (this.message.length > 3) {
+                this.frasi.push(this.message);                
+            }
+        }
+    },
+    created(){
+
+    }
+});
